fcae3df376d92f5aa2104858e361ed611b8f3f30
melisa_dominguez <melisa_dominguez@epam.com>
2024-03-06 15:39:58 -0500

    Updated: API Dropbox token

--- dropboxAPI/api.test.js
+++ dropboxAPI/api.test.js
@@ -1,13 +1,10 @@
 // This corrected code snippet is in JavaScript.
 const axios = require('axios');
 const chai = import ('chai');
+const token = 'sl.Bw5aebg30i_bYpfP5e0CJ0crxTuf9Nf7bks-X_y1QCdlldSXUdFhmEYCKE3aB9PJi6AMyJcoz4T47rxkZA8K_uTIsTw0ZMOE2tHqI9Jy5YRJntltQKm9yxoMuP5vnI4jvdwuL8TDhn3Dw9AqDsCsQHM'
 describe('DropboxAPI', async () => {
     it('Upload a file to the API dropbox account', async () => {
-
         const fileContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-
-        const token = 'sl.BwxWf8QSuDoHv8-jYMSofVty7eTmJTcbpjQgg5NJHI-PDQ1ECKdCTJMR0ZbIXV5KSOb13A_H7leV1lVTyNVooBBIEwahCYkDhBxT6TU28MryhLq1mPwSvBw-Hsil-743Nnt99_YRPDFtDIx0LYfS2ec'
-
         const response = await axios.post('https://content.dropboxapi.com/2/files/upload', fileContent,
             {
                 headers: {
@@ -22,13 +19,11 @@ describe('DropboxAPI', async () => {
                     'Content-Type': 'application/octet-stream'
                 }
             });
-        //console.log(await response.data);
         (await chai).expect(response.data.name).to.equal('test10.txt')
 
     })
     it('Obtain metadata file', async () => {
 
-        const token = 'sl.BwxWf8QSuDoHv8-jYMSofVty7eTmJTcbpjQgg5NJHI-PDQ1ECKdCTJMR0ZbIXV5KSOb13A_H7leV1lVTyNVooBBIEwahCYkDhBxT6TU28MryhLq1mPwSvBw-Hsil-743Nnt99_YRPDFtDIx0LYfS2ec'
         const data = {
             "include_deleted": false,
             "include_has_explicit_shared_members": false,
@@ -43,12 +38,10 @@ describe('DropboxAPI', async () => {
                     'Content-Type': 'application/json'
                 }
             });
-        //console.log(await response.headers);
         (await chai).expect(response.headers).to.have.property('content-type')
     })
     it('Erase file', async () => {
 
-        const token = 'sl.BwxWf8QSuDoHv8-jYMSofVty7eTmJTcbpjQgg5NJHI-PDQ1ECKdCTJMR0ZbIXV5KSOb13A_H7leV1lVTyNVooBBIEwahCYkDhBxT6TU28MryhLq1mPwSvBw-Hsil-743Nnt99_YRPDFtDIx0LYfS2ec'
         const data = {
             "path": "/test10.txt"
         }
@@ -60,7 +53,6 @@ describe('DropboxAPI', async () => {
                     'Content-Type': 'application/json'
                 }
             });
-        //console.log(await response.status);
         (await chai).expect(response.status).to.equal(200)
     })
 
